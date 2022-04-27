6bc2423edc2892d38a5be7c60bce3c2a12383b46
FunG <99120041+funglkf@users.noreply.github.com>
2022-04-27 22:55:42 +0800

    add leaflet

---
+++ src/lib/LeafletMap.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { onMount } from 'svelte';
+    import { browser } from '$app/env';
+
+    onMount(async () => {
+        if(browser) {
+            const leaflet = await import('leaflet');
+
+            const map = leaflet.map('map').setView([51.505, -0.09], 13);
+
+            leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            leaflet.marker([51.5, -0.09]).addTo(map)
+                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+                .openPopup();
+        }
+    });
+</script>
+
+
+<main>
+    <div class="h-noHeader-noFooter-noTitle" id="map"></div>
+</main>
+
+<style>
+    @import 'leaflet/dist/leaflet.css';
+</style>
---
+++ src/routes/map.svelte
@@ -0,0 +1,8 @@
+<script>
+	import LeafletMap from '$lib/LeafletMap.svelte';
+</script>
+
+<h1 class="px-6 text-lg font-semibold">Leaflet Map</h1>
+<div class="mx-5">
+    <LeafletMap />
+</div>
